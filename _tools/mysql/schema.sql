78c5b018986d23d62e26105ec076302b3d87871b
taiti09 <54522966+taiti09@users.noreply.github.com>
2022-10-12 10:03:35 +0900

    Update schema.sql
    DDL修正

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE `user`
     `modified_at` DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
     PRIMARY KEY (`id`),
     UNIQUE KEY `uix_name` (`name`) USING BTREE
-) Engine=InnoDB DEFAULT CHARSET=utfmb4 COMMENT='ユーザー';
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
 
 CREATE TABLE `task`
 (
