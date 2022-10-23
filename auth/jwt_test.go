10a4ee7b95d3e03a4dfb0d2c6125ed5529d74b79
taiti09 <taitiabe1997@gmail.com>
2022-10-23 21:50:18 +0900

    authパッケージに対するテストコード実装完了

--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -3,12 +3,28 @@ package auth
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 	"github.com/taiti09/go_app_handson/clock"
 	"github.com/taiti09/go_app_handson/entity"
+	"github.com/taiti09/go_app_handson/store"
 	"github.com/taiti09/go_app_handson/testutil/fixture"
 )
+
+type FixedTommorowClocker struct {}
+
+func (c FixedTommorowClocker) Now() time.Time {
+	return clock.FixedClocker{}.Now().Add(24*time.Hour)
+}
 
 func TestEmbed(t *testing.T) {
 	want := []byte("------------BEGIN PUBLIC KEY------------")
@@ -39,5 +55,105 @@ func TestJWTer_GenerateToken(t *testing.T) {
 	}
 	if len(got) == 0 {
 		t.Errorf("token is empty")
+	}
+}
+
+func Test_JWTer_GetToken(t *testing.T) {
+	t.Parallel()
+
+	c := clock.FixedClocker{}
+	want, err := jwt.NewBuilder().JwtID(uuid.New().String()).Issuer(`github.com/taiti09/go_app_handson/`).Subject("access_token").IssuedAt(c.Now()).Expiration(c.Now().Add(30*time.Minute)).Claim(RoleKey,"test").Claim(UserNameKey,"test_user").Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pkey, err := jwk.ParseKey(rawPrivKey,jwk.WithPEM(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+	signed, err := jwt.Sign(want,jwt.WithKey(jwa.RS256,pkey))
+	if err != nil {
+		t.Fatal(err)
+	}
+	userID :=entity.UserID(20)
+	ctx := context.Background()
+	moq := &StoreMock{}
+	moq.LoadFunc = func(ctx context.Context, key string) (entity.UserID, error) {
+		return userID, nil
+	}
+	sut, err := NewJWTer(moq,c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest(http.MethodGet,`https://github.com/taiti09`,nil)
+	req.Header.Set(`Authorozation`,fmt.Sprintf(`Bearer %s`, signed))
+	got, err := sut.GetToken(ctx,req)
+	if err != nil {
+		t.Fatalf("want no error, but got %v",err)
+	}
+	if !reflect.DeepEqual(got,want) {
+		t.Errorf("Gettoken() got = %v, want %v", got, want)
+	}
+}
+
+func TestJWTer_GetToken_NG(t *testing.T){
+	t.Parallel()
+
+	c := clock.FixedClocker{}
+	tok, err := jwt.NewBuilder().JwtID(uuid.New().String()).Issuer(`github.com/taiti09/go_app_handson/`).Subject("access_token").IssuedAt(c.Now()).Expiration(c.Now().Add(30*time.Minute)).Claim(RoleKey,"test").Claim(UserNameKey,"test_user").Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pkey, err := jwk.ParseKey(rawPrivKey,jwk.WithPEM(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+	signed, err := jwt.Sign(tok,jwt.WithKey(jwa.RS256,pkey))
+	if err != nil {
+		t.Fatal(err)
+	}
+	type moq struct {
+		userID entity.UserID
+		err error
+	}
+	tests := map[string]struct {
+		c clock.Clocker
+		moq moq
+	}{
+		"expure": {
+			c: FixedTommorowClocker{},
+		},
+		"notFoundInstore": {
+			c: clock.FixedClocker{},
+			moq: moq{
+				err: store.ErrNotFound,
+			},
+		},
+	}
+	for n, tt := range tests {
+		tt := tt
+		t.Run(n,func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+			moq := &StoreMock{}
+			moq.LoadFunc = func(ctx context.Context, key string) (entity.UserID, error) {
+				return tt.moq.userID, tt.moq.err
+			}
+			sut, err := NewJWTer(moq,tt.c)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			req := httptest.NewRequest(http.MethodGet,`https://github.com/taiti09`,nil)
+			req.Header.Set(`Authorozation`,fmt.Sprintf(`Bearer %s`, signed))
+			got, err := sut.GetToken(ctx,req)
+			if err == nil {
+				t.Fatalf("want no error, but got nil")
+			}
+			if got != nil {
+				t.Errorf("want nil, but got %v", got)
+			}
+		})
 	}
 }
