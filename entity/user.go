37f808378d28e2fc2176ef83e14be7faf075e77b
taiti09 <taitiabe1997@gmail.com>
2022-10-26 23:21:53 +0900

    ログイン認証の実装

--- entity/user.go
+++ entity/user.go
@@ -1,6 +1,10 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type UserID int64
 
@@ -11,4 +15,8 @@ type User struct {
 	Role string `json:"role" db:"role"`
 	Created_at time.Time `json:"created_at" db:"created_at"`
 	Modified_at time.Time `json:"modified_at" db:"modified_at"`
+}
+
+func (u *User) ComparePassword(pw string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password),[]byte(pw))
 }
--- service/interface.go
+++ service/interface.go
@@ -7,7 +7,7 @@ import (
 	"github.com/taiti09/go_app_handson/store"
 )
 
-//go:generate go run github.com/matryer/moq -out moq_test.go . TaskAdder TaskListner
+//go:generate go run github.com/matryer/moq -out moq_test.go . TaskAdder TaskListner UserRegister
 type TaskAdder interface {
 	AddTask(ctx context.Context, db store.Execer, t *entity.Task) error
 }
@@ -18,4 +18,12 @@ type TaskListner interface {
 
 type UserRegister interface {
 	RegisterUser(ctx context.Context, db store.Execer, u *entity.User) error
+}
+
+type UserGetter interface {
+	GetUser(ctx context.Context, db store.Queryer, name string) (*entity.User, error)
+}
+
+type TokenGenerator interface {
+	GenerateToken(ctx context.Context, u entity.User) ([]byte, error)
 }
