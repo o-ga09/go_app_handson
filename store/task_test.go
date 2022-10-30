1798bc84dc15c960ed338d6222d7d909532814fe
taiti09 <taitiabe1997@gmail.com>
2022-10-30 23:01:55 +0900

    fix: 誤字

--- store/task_test.go
+++ store/task_test.go
@@ -56,7 +56,7 @@ func TestRepository_AddTask(t *testing.T) {
 	t.Cleanup(func() { _ = db.Close()})
 	mock.ExpectExec(
 		// エスケープが必要
-		`INSERT INTO task \(user_id, title, status, created, modified\) VALUES \(\?, \?, \?, \?, \?\)`,
+		`INSERT INTO task \(user_id, title, status, created_at, modified_at\) VALUES \(\?, \?, \?, \?, \?\)`,
 	).WithArgs(okTask.UserID, okTask.Title, okTask.Status, c.Now(), c.Now()).
 		WillReturnResult(sqlmock.NewResult(wantID, 1))
 	// mock.ExpectExec(
